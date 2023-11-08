f9818b1bb0889412aea42f01caa6c5741e576b06
Sudip Roy <sudip.roy@zalando.de>
2023-11-08 20:08:45 +0100

    pass 3rd stage

--- app/main.js
+++ app/main.js
@@ -3,10 +3,27 @@ const net = require("net");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
+const parseHttpData = (data) => {
+    const dataTransformed = data.toString().split('\r\n');
+    const req = {};
+    const [firstLine, secondLine, thirdLine] = dataTransformed;
+    const sections = firstLine.split(' ');
+    req.method = sections[0];
+    req.path = sections[1];
+    req.httpVersion = sections[2];
+    const headers = {};
+    headers.host = secondLine.split(' ')[1];
+    headers.userAgent = thirdLine.split(' ')[1];
+    req.headers = headers;
+    return req;
+}
+
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
     socket.on('data', data=> {
-        socket.write('HTTP/1.1 200 OK\r\n\r\n');
+        const req = parseHttpData(data);
+        const responseCode = req.path === '/' ? '200 OK' : '404 Not Found';
+        socket.write(`HTTP/1.1 ${responseCode}\r\n\r\n`);
         socket.end();
     });
   socket.on("close", () => {
