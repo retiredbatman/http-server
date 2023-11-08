ec289a5d8b933376cb4c93948728ae8afb199db3
Sudip Roy <sudip.roy@zalando.de>
2023-11-08 19:49:54 +0100

    pass 2nd stage

--- app/main.js
+++ app/main.js
@@ -5,6 +5,10 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+    socket.on('data', data=> {
+        socket.write('HTTP/1.1 200 OK\r\n\r\n');
+        socket.end();
+    });
   socket.on("close", () => {
     socket.end();
     server.close();
